05687c2c3f16813319a87c787c66286551745eca
Matthew Davis <matthew@matthewdavis.io>
2024-01-03 21:13:31 -0500

    bump

--- test/e2e.go
+++ test/e2e.go
@@ -1,59 +1,59 @@
 package test
 
-import (
-	"context"
-	"testing"
+// import (
+// 	"context"
+// 	"testing"
 
-	"github.com/nvr-ai/go-rabbitmq/producer"
-	"github.com/stretchr/testify/suite"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/rabbitmq"
-)
+// 	"github.com/nvr-ai/go-rabbitmq/producer"
+// 	"github.com/stretchr/testify/suite"
+// 	"github.com/testcontainers/testcontainers-go"
+// 	"github.com/testcontainers/testcontainers-go/modules/rabbitmq"
+// )
 
-type ProducerTestSuite struct {
-	suite.Suite
-	Endpoint string
-}
-
-func TestTenantSuite(t *testing.T) {
-	suite.Run(t, new(ProducerTestSuite))
-}
-
-// func (s *ProducerTestSuite) SetupSuite() {
-// 	println("SetupSuite")
+// type ProducerTestSuite struct {
+// 	suite.Suite
+// 	Endpoint string
 // }
 
-// func (s *ProducerTestSuite) TearDownSubTest() {
-// 	println("TearDownSubTest")
+// func TestTenantSuite(t *testing.T) {
+// 	suite.Run(t, new(ProducerTestSuite))
 // }
 
-func (s *ProducerTestSuite) TestNewConsumer() {
-	ctx := context.Background()
+// // func (s *ProducerTestSuite) SetupSuite() {
+// // 	println("SetupSuite")
+// // }
 
-	rabbitmqContainer, err := rabbitmq.RunContainer(ctx,
-		testcontainers.WithImage("rabbitmq:3.10-management"),
-		rabbitmq.WithAdminUsername("guest"),
-		rabbitmq.WithAdminPassword("guest"),
-	)
+// // func (s *ProducerTestSuite) TearDownSubTest() {
+// // 	println("TearDownSubTest")
+// // }
 
-	if err != nil {
-		s.T().Error(err)
-	}
+// func (s *ProducerTestSuite) TestNewConsumer() {
+// 	ctx := context.Background()
 
-	// Clean up the container
-	defer func() {
-		if err := rabbitmqContainer.Terminate(ctx); err != nil {
-			s.T().Error(err)
-		}
-	}()
+// 	rabbitmqContainer, err := rabbitmq.RunContainer(ctx,
+// 		testcontainers.WithImage("rabbitmq:3.10-management"),
+// 		rabbitmq.WithAdminUsername("guest"),
+// 		rabbitmq.WithAdminPassword("guest"),
+// 	)
 
-	amqpUrl, err := rabbitmqContainer.AmqpURL(ctx)
-	if err != nil {
-		s.T().Error(err)
-	}
+// 	if err != nil {
+// 		s.T().Error(err)
+// 	}
 
-	println("RabbitMQ running at %s", amqpUrl)
+// 	// Clean up the container
+// 	defer func() {
+// 		if err := rabbitmqContainer.Terminate(ctx); err != nil {
+// 			s.T().Error(err)
+// 		}
+// 	}()
 
-	producer := &producer.Producer{}
-	producer.Connect(amqpUrl)
-}
+// 	amqpUrl, err := rabbitmqContainer.AmqpURL(ctx)
+// 	if err != nil {
+// 		s.T().Error(err)
+// 	}
+
+// 	println("RabbitMQ running at %s", amqpUrl)
+
+// 	producer := &producer.Producer{}
+// 	producer.Connect(amqpUrl)
+// }
