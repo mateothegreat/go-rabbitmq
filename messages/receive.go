3bb9c1c9bf455d21e987d4c58c96c27d4a9220d8
Matthew Davis <matthew@matthewdavis.io>
2024-01-25 18:02:53 -0500

    bump

--- messages/receive.go
+++ messages/receive.go
@@ -1,8 +1,8 @@
 // Package messages contains structs and functions for working with RabbitMQ messages.
 package messages
 
-type Receive struct {
-	Namespace string  `json:"namespace"`
-	Body      []uint8 `json:"body"`
-	Status    string  `json:"status"`
+type Receive[T any] struct {
+	Namespace string `json:"namespace"`
+	Body      T      `json:"body"`
+	Status    string `json:"status"`
 }
